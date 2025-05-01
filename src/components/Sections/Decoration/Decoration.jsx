2311bb3651a80101b69d3c36bbc9c279976e6e87
Asya I <as.izmaylova@gmail.com>
2025-05-01 17:30:57 +0300

    made footer, decoration sections, some changes in media css in lobby

--- src/components/Sections/Decoration/Decoration.jsx
+++ src/components/Sections/Decoration/Decoration.jsx
@@ -1,10 +1,43 @@
 import styles from "./Decoration.module.css"
 
 import React from 'react'
+import pic1 from "./images/Frame 1956.png"
+import pic2 from "./images/image 210.png"
+import pic3 from "./images/Frame 1953.png"
 
 function Decoration() {
   return (
-    <div>Decoration</div>
+    <section className={styles.decorSection}>
+      <h1>Отделка от застройщика</h1>
+      <h4>Экономьте на стоимости ремонта, покупая квартиру с готовой отделкой</h4>
+      <div className={styles.cardsContainer}>
+        <div className={styles.cardBlock}>
+          <img src={pic1} alt="otdelka pic1" />
+          <h5>Готовый дизайн-проект</h5>
+          <p>Воспользуйтесь возможностью сразу же заняться
+            меблировкой и благоустройством новой квартиры,
+            как только получите ключи
+          </p>
+        </div>
+
+        <div className={styles.cardBlock}>
+          <img src={pic2} alt="otdelka pic2" />
+          <h5>Чистота и тишина, без шума и пыли</h5>
+          <p>Забудьте шум от "бесконечного ремонта" соседей 
+            и лифт, декорированный защитными материалами
+          </p>
+        </div>
+
+        <div className={styles.cardBlock}>
+          <img src={pic3} alt="otdelka pic3" />
+          <h5>Выгода за счет оптовых закупок</h5>
+          <p>Благодаря оптовым закупкам застройщика,
+            вы получаете возможность сэкономить на стоимости
+            ремонта
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Sections/Footer/Footer.jsx
+++ src/components/Sections/Footer/Footer.jsx
@@ -1,10 +1,71 @@
 import styles from "./Footer.module.css"
 
 import React from 'react'
+import mainLogo from "./images/mainlogo.png"
+import estesLogo from "./images/logo.png"
+import map from "./images/map.png"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <section className={styles.footerSection}>
+      <div className={styles.footerContainer}>
+        <div className={styles.headerBlock}>
+          <h2>Жилой комплекс "West Garden" — все преимущества жизни на природе</h2>
+          <img src={mainLogo} alt="west garden logo" />
+        </div>
+        <hr className={styles.mainLine}/>
+        <div className={styles.footerBlock}>
+          <div className={styles.textBlock}>
+            <img src={estesLogo} alt="estes logo" />
+            <hr />
+            <div className={styles.adressBlock}>
+              <p>Офис продаж</p>
+              <p>Москва, Локал наб. 2/4, строение 17, офис 308</p>
+            </div>
+            <hr />
+            <div className={styles.telBlock}>
+              <p>Телефон</p>
+              <div>
+                <p>+7 495 805 19 34</p>
+                <span>· работаем</span>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.picDiv}>
+            <img className={styles.map} src={map} alt="map" />
+          </div>
+
+        </div>
+      </div>
+      {/* <div className={styles.headerBlock}>
+        <h2>Жилой комплекс "West Garden" — все преимущества жизни на природе</h2>
+        <img src={mainLogo} alt="west garden logo" />
+      </div>
+      <hr />
+      <div className={styles.footerBlock}>
+        <div className={styles.textBlock}>
+          <img src={estesLogo} alt="estes logo" />
+          <hr />
+          <div className={styles.adressBlock}>
+            <p>Офис продаж</p>
+            <p>Москва, Локал наб. 2/4, строение 17, офис 308</p>
+          </div>
+          <hr />
+          <div className={styles.telBlock}>
+            <p>Телефон</p>
+            <div>
+              <p>+7 495 805 19 34</p>
+              <span>· работаем</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img className={styles.map} src={map} alt="map" />
+        </div>
+
+      </div> */}
+    </section>
   )
 }
 
